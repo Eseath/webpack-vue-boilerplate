fa7c7872a83881518702f4a531c545a2abd12a41
Ruslan <ruslan.a94@yandex.com>
2019-10-04 23:18:42 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Application',
+
+    data: () => ({
+        title: 'Hello, World!',
+    }),
+};
+</script>
+
+<style lang="scss">
+h1 {
+    text-align: center;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const { resolve } = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
+    entry: './src/index.js',
+
+    output: {
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'app.js',
+    },
+
+    devServer: {
+        contentBase: resolve(__dirname, 'public'),
+        historyApiFallback: true,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+
+    resolve: {
+        alias: {
+            components: resolve(__dirname, 'src/components/'),
+            views: resolve(__dirname, 'src/views/'),
+        },
+        extensions: ['.js', '.vue', '.json'],
+    },
+
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+    }
+
+    if (argv.mode === 'production') {
+        config.devtool = 'none';
+    }
+
+    return config;
+};
